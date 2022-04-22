242a655ddef5cc47a8dd577d2fafae87928e10cc
Luis Bazaldua <LEBazaldua@student.fullsail.edu>
2022-04-22 16:38:01 -0400

    Add project files.

---
+++ PressureAlarm/PressAlarm.cs
@@ -0,0 +1,30 @@
+﻿namespace PressureAlarm
+{
+    public class PressAlarm
+    {
+
+        private const double lowPressureThreshold = 15;
+        private const double highPressureThreshold = 32;
+        ISensor Isensor = null;
+        bool alarm = false;
+
+        public PressAlarm(ISensor sensor)
+        {
+            Isensor = sensor;
+        }
+
+        public void Check()
+        {
+            double pressure = Isensor.QueryHardwareForPsiValue();
+            if (pressure < lowPressureThreshold || highPressureThreshold < pressure)
+            {
+                alarm = true;
+            }
+        }
+
+        public bool Alarm
+        {
+            get { return alarm; }
+        }
+    }
+}
